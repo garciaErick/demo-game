226fa529637fb333fa3aa4248968941f2e55060e
Erick Garcia <me@erick-garcia.com>
2019-09-16 09:39:15 -0700

    Starting commit, checking import in another computer

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController2D characterController;
+	float horizontalMove = 0f;
+	bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+			horizontalMove = Input.GetAxisRaw("Horizontal");
+			jump = Input.GetButton("Jump") ? true : false;
+        
+    }
+
+		private void FixedUpdate() {
+			characterController.Move(horizontalMove, false, jump);
+			jump = false;
+			
+		}
+}
